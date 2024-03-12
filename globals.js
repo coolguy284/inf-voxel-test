f8b072828afea429f23ecb73ecf93c5e29c26cee
coolguy284 <coolguy284v64@gmail.com>
2024-03-12 02:36:24 -0400

    start menu page switching
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ globals.js
@@ -0,0 +1,17 @@
+let mainPageManager = new PageManager({
+  'Main Menu': {
+    htmlElem: scene_main_menu,
+  },
+  'Singleplayer Menu': {
+    htmlElem: singleplayer_menu,
+  },
+  'Multiplayer Menu': {
+    htmlElem: multiplayer_menu,
+  },
+  'Options Menu': {
+    htmlElem: options_menu,
+  },
+  'Game': {
+    htmlElem: scene_game,
+  },
+}, 'Main Menu');
