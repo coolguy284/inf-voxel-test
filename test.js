f63af34ca12b9416c539c915aa6f561058cd7ae3
coolguy284 <coolguy284v64@gmail.com>
2024-03-12 07:41:39 -0400

    make basic worldpos class
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- test.js
+++ test.js
@@ -38,7 +38,39 @@ function test_chunk_manager() {
   console.log('chunk manager test finished');
 }
 
+function test_world_pos() {
+  console.log('world pos test');
+  
+  let worldPos1 = new WorldPos();
+  worldPos1.setBlockX(0n);
+  worldPos1.setBlockY(-1n);
+  console.log(worldPos1.toString());
+  
+  let worldPos2 = new WorldPos();
+  worldPos2.setBlockX(0n);
+  worldPos2.setBlockY(-1n);
+  worldPos2.setFracY(0.1);
+  console.log(worldPos2.toString());
+  
+  let worldPos3 = new WorldPos();
+  worldPos3.setBlockX(1n);
+  worldPos3.setFracX(0.1);
+  worldPos3.setBlockY(-2n);
+  worldPos3.setFracY(0.1);
+  console.log(worldPos3.toString());
+  
+  let worldPos4 = new WorldPos();
+  worldPos4.setBlockX(1n);
+  worldPos4.setFracX(1.1);
+  worldPos4.setBlockY(-2n);
+  worldPos4.setFracY(0.1);
+  console.log(worldPos4.toString());
+  
+  console.log('world pos test finished');
+}
+
 function test_all() {
   test_chunk();
   test_chunk_manager();
+  test_world_pos();
 }
